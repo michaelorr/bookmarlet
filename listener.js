b5bac9f0b35b23771410e6c7cb20702eaaf02d02
Michael Orr <michael.orr@coxinc.com>
2013-11-21 14:16:07 -0500

    final?

--- listener.js
+++ listener.js
@@ -29,9 +29,9 @@ recognition.onend = function(){
 
 recognition.onresult = function(event){
     for (var i = event.resultIndex; i < event.results.length; i++) {
-        //if (event.results[i].isFinal) {
+        if (event.results[i].isFinal) {
             highlight(event.results[i][0].transcript);
-        //}
+        }
     }
 }
 
